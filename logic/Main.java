ac27323a9d3d8f9b878b92b7519141e83d4323e2
Andy Madrigal <andymadrigal@estudiantec.cr>
2023-03-05 21:53:06 -0600

    dummy update
    testing git functionality

--- logic/Main.java
+++ logic/Main.java
@@ -2,6 +2,6 @@ package logic;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("PROFE BASES MOTIVADO");
+        System.out.println("PROFE BASES MOTIVADO!!!!! :D ");
     }
 }
